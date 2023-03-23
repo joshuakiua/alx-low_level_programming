413c22a8508e5e25da1e26d59583a35c90774d1b
joshuakiua <joshuamwendwa91@gmail.com>
2023-03-23 06:17:39 -0700

    r your changes. Lines starting

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
 * main - Test function for positive or negative
-* Return: always 0 (success)
+* Return: 0
 */
 int main(void)
 {
 	int i;
-;
+
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
